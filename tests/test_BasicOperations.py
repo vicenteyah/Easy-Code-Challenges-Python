bcfef6732a8828782f8c8ececc7cec0e562ff95e
Vicente Yah <vicente.yah@accenture.com>
2024-08-30 11:35:27 -0600

    outputs corrected in testCase section

--- tests/test_BasicOperations.py
+++ tests/test_BasicOperations.py
@@ -8,5 +8,5 @@ class BasicOperationsTest(unittest.TestCase):
         test1 = BasicOperations([3,5,15,7,9])
         test2 = BasicOperations([6, 10, 30, 12, 25])
         
-        self.assertEqual(test1.calculate_result(),12) # Final result: 12 - 5 + 5 = 12 
-        self.assertEqual(test2.calculate_result(),-17) # 18 - 35 + 0 = -17
+        self.assertEqual(test1.calculate_result(),12) # 12 - 5 + 5 = 12 
+        self.assertEqual(test2.calculate_result(),-17) # 48 - 35 + 0 = 13 
